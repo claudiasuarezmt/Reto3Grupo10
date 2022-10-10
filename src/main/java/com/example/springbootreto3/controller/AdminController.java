5cc6c65455561ba03118b43d6ade964cb69c7bf7
Diana Saltarin <dsaltarin@gascaribe.com>
2022-10-10 13:20:16 -0500

    admin + correción library service

---
+++ src/main/java/com/example/springbootreto3/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.example.springbootreto3.controller;
+
+import com.example.springbootreto3.entidades.Admin;
+import com.example.springbootreto3.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/Admin")
+public class AdminController {
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping("/all")
+    public List<Admin> getAll(){
+        return adminService.getAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<Admin> getById(@PathVariable("id") int adminId){
+        return adminService.getById(adminId);
+    }
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Admin save(@RequestBody Admin ad){
+        return adminService.save(ad);
+    }
+}
