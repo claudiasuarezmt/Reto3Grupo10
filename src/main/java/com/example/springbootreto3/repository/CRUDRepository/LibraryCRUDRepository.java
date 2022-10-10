113b1fcbc8504a78555eddd581d499b164d2de85
Diana Saltarin <dsaltarin@gascaribe.com>
2022-10-09 20:56:25 -0500

    library

---
+++ src/main/java/com/example/springbootreto3/repository/CRUDRepository/LibraryCRUDRepository.java
@@ -0,0 +1,2 @@
+package com.example.springbootreto3.repository.CRUDRepository;public interface LibraryCRUDRepository {
+}
---
+++ src/main/java/com/example/springbootreto3/services/LibraryService.java
@@ -0,0 +1,26 @@
+package com.example.springbootreto3.services;
+
+import com.example.springbootreto3.entidades.Library;
+import com.example.springbootreto3.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LibraryService {
+    @Autowired
+    private LibraryRepository libraryRepository;
+
+    public List<Library> getAll(){
+        return (List<Library>)libraryRepository.getAll();
+    }
+    public Library save(Library lb){
+
+        return libraryRepository.save(lb);
+    }
+    public Optional<Library> getById(int idLibrary){
+        return libraryRepository.getById(idLibrary);
+    }
+}
