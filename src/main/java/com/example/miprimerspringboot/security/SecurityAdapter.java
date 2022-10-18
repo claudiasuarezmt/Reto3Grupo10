1ae468ca692e58a06ac5c2989eec247b6e89d9f7
Bryan Espinosa <bryan.espinosa.mt@usa.edu.co>
2022-10-18 07:41:04 -0500

    despliegue

--- src/main/java/com/example/miprimerspringboot/security/SecurityAdapter.java
+++ src/main/java/com/example/miprimerspringboot/security/SecurityAdapter.java
@@ -1,6 +1,7 @@
 package com.example.miprimerspringboot.security;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
